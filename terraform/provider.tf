11da29808d0cd89291582b9311274b0a704f19f2
Gersi Muca <gersi.muca@lhind.dlh.de>
2026-02-06 23:30:57 +0100

    setting up the provider for the services, location and access key

---
+++ terraform/provider.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  s3_use_path_style           = true
+
+  endpoints {
+    dynamodb   = "http://localhost:4566"
+    lambda     = "http://localhost:4566"
+    apigateway = "http://localhost:4566"
+    iam        = "http://localhost:4566"
+  }
+}
